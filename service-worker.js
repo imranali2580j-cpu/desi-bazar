ca8ed331df62dde167b2753a8de9dd6fe8f18f3d
imranali2580j-cpu <imranali2580j@gmail.com>
2026-07-16 00:01:41 +0300

    Add service worker installation and activation events

--- service-worker.js
+++ service-worker.js
@@ -1,3 +1,13 @@
+const CACHE_NAME = "desi-bazar-v1";
+
+self.addEventListener("install", function(event){
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", function(event){
+  self.clients.claim();
+});
+
 self.addEventListener("fetch", function(event){
 
 });
