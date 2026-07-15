a894bc1a588d290043e244f200374569203f4c0a
imranali2580j-cpu <imranali2580j@gmail.com>
2026-07-15 23:23:46 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,54 @@
+let cart = [];
+let total = 0;
+
+function addToCart(product, price) {
+    cart.push(product);
+    total += price;
+
+    document.getElementById("cartList").innerHTML = cart.join("<br>");
+    document.getElementById("totalPrice").innerHTML = total;
+}
+
+function placeOrder() {
+    let name = document.getElementById("customerName").value.trim();
+    let phone = document.getElementById("customerPhone").value.trim();
+    let address = document.getElementById("customerAddress").value.trim();
+let area = document.getElementById("customerArea").value;
+    if (cart.length === 0) {
+        alert("কার্টে কোনো পণ্য নেই!");
+        return;
+    }
+
+    if (name === "" || phone === "" || address === "") {
+        alert("সব তথ্য পূরণ করুন!");
+        return;
+    }
+
+    let orders = JSON.parse(localStorage.getItem("orders")) || [];
+
+    let order = {
+        id: orders.length + 1,
+        name: name,
+        phone: phone,
+        address: address,
+      area: area,
+        products: cart,
+        total: total,
+        date: new Date().toLocaleString()
+    };
+
+    orders.push(order);
+    localStorage.setItem("orders", JSON.stringify(orders));
+
+    alert("✅ আপনার অর্ডার সফলভাবে নেওয়া হয়েছে।");
+
+    cart = [];
+    total = 0;
+
+    document.getElementById("cartList").innerHTML = "কোনো পণ্য যোগ করা হয়নি।";
+    document.getElementById("totalPrice").innerHTML = "0";
+
+    document.getElementById("customerName").value = "";
+    document.getElementById("customerPhone").value = "";
+    document.getElementById("customerAddress").value = "";
+}
